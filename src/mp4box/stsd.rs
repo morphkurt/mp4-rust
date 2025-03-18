37bc464dece42e71a90e03a2045f3be3f6f572ee
damitha <damitha.gunawardena@gmail.com>
2025-03-18 23:07:02 +1100

    .

--- src/mp4box/stsd.rs
+++ src/mp4box/stsd.rs
@@ -154,6 +154,8 @@ impl<W: Write> WriteBox<&mut W> for StsdBox {
             mp4a.write_box(writer)?;
         } else if let Some(ref tx3g) = self.tx3g {
             tx3g.write_box(writer)?;
+        }else if let Some(ref opus) = self.opus {
+            opus.write_box(writer)?;
         }
 
         Ok(size)
